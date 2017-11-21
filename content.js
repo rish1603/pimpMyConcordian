600085f7210c4f063784184fa7affe620453ff0a
rish1603 <rish1603@gmail.com>
2017-11-21 22:31:37 +0000

    removed space method

--- content.js
+++ content.js
@@ -52,7 +52,7 @@ function manipulateDom(callback) {
             fragment.appendChild(code);
         });
         actualDom.appendChild(fragment);
-        callback(actualToRemove, actualDom, manipulateDom); //need to pass in an array of doms, then delete them in a loop in the callback
+        callback(actualToRemove, actualDom, manipulateDom); 
     }
     else {
         var preToConvert = document.getElementsByTagName('pre')
@@ -79,16 +79,6 @@ function manipulateDom(callback) {
             s.parentNode.removeChild(s);
         };
 
-        //call new function here -> function needs to get to end of html after it has all been pimped
-        // make it use updated innerHTML
-        failures = document.getElementsByClassName('rishiDiv');
-        myDivs = document.getElementsByClassName('expected');
-        console.log(myDivs[0].innerHTML)
-
-        for (let x = 0; x < myDivs.length; x++) {
-            failures[x].innerHTML = fillLines(failures[x].innerHTML, myDivs[x].innerHTML);
-        }
-
         return;
     }
 }
@@ -101,59 +91,4 @@ function removeActual(actualToRemove, actualDom, callback) {
     counter++;
     callback(removeActual);
 }
-
-function fillLines(string, myDiv) {
-    var diffToLongest;
-    var longestLine = 0;
-    var lastLinePosition;
-    var previousIndex = 0;
-    var spacesString = 'axax';
-    var index = myDiv.length;
-    var lineCount;
-    var charPosition;
-    var arrayOfIndex = []; //stores the indexes at where the line ends
-    var numSpaces = [];
-
-    for (lineCount = -1, _index = index; index != -1; lineCount++) {
-
-        index = string.indexOf('&gt;\n', index + 1) //find position of line break
-        charPosition = index - previousIndex; //find the difference i.e. number of chars on this line
-
-        arrayOfIndex.push(index);
-
-        if (charPosition > longestLine) { //calculate line with the most chars
-            longestLine = charPosition;
-        }
-        previousIndex = index; //update previous
-    }
-
-    for (lineCount = -1, _index = index; index != -1; lineCount++) {
-
-        index = string.indexOf('&gt;\n', index + 1) //find position of line break
-        charPosition = index - previousIndex; //find the difference i.e. number of chars on this line
-        console.log(charPosition);
-        diffToLongest = longestLine - charPosition;
-        numSpaces.push(diffToLongest); // (int) diff to longest line
-
-        previousIndex = index; //update previous
-    }
-    console.log(numSpaces);
-
-    arrayOfIndex.splice(0,1); //remove first element
-    arrayOfIndex.pop(arrayOfIndex.length - 1) //remove last element
-
-    for(let x = arrayOfIndex.length - 1; x >= 0; x--) {
-        string = string.insert(arrayOfIndex[x] + 4, spacesString);
-    }
-
-    return string;
-}
-
-String.prototype.insert = function (index, string) {
-    if (index > 0)
-        return this.substring(0, index) + string + this.substring(index, this.length);
-    else
-        return string + this;
-};
-
 
