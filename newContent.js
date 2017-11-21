7639ac70399fd317a41d96605c01a61984e29974
rish1603 <rish1603@gmail.com>
2017-11-21 22:48:13 +0000

    updated manifest to include new icon

--- newContent.js
+++ newContent.js
@@ -1,100 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var jsdiff = require('diff');
-var counter = 0;
-var orderedList = document.getElementsByTagName('ol')
-var tdArray = document.getElementsByTagName('td')
-var numFails = document.getElementsByClassName('failure').length - 1; //number of fails starting at 0 ;)
-var failures, myDivs = [];
-
-chrome.runtime.onMessage.addListener(
-    function(request, sender) {
-        if (request.message == "removeDiff") {
-            var myDivs = [];
-            manipulateDom(removeActual);
-        }
-    }
-);
-
-function manipulateDom(callback) {
-
-    if (counter <= numFails) {
-
-        orderedList[0].parentNode.removeChild(orderedList[0]) //remove lists
-
-        var actualDom = document.getElementsByClassName('failure')[0]; //points to div
-
-        var actualToRemove = document.getElementsByClassName('failure')[0].getElementsByClassName('actual')[0]; //points to pre
-        var actual = document.getElementsByClassName('failure')[0].getElementsByClassName('actual')[0].textContent;
-        var expected = document.getElementsByClassName('failure')[0].getElementsByClassName('expected')[0].textContent;
-        document.getElementsByClassName('failure')[0].getElementsByClassName('expected')[0].style.textDecoration = "none"; //remove line-through expected
-
-        var actualDomArray = document.getElementsByClassName('failure'); //points to div
-        var color = '',
-            code = null;
-        var diff = jsdiff.diffLines(actual, expected, {"ignoreWhitespace": false, "newlineIsToken": false}),
-            description = document.getElementById('description'),
-
-            fragment = document.createDocumentFragment();
-        diff.forEach(function(part){
-            // green for additions, red for deletions
-            // grey for common parts
-            code = document.createElement('code');
-            color = part.added ? '#bbffbb' :
-                part.removed ? '#ffbbb0' : '#f5f2f0';
-            if (part.added || part.removed) {
-                code.classList.add('language-diff');
-            }
-            else {
-                code.classList.add('language-XML');
-            }
-            code.style.backgroundColor = color;
-            code.appendChild(document
-                .createTextNode(part.value));
-            fragment.appendChild(code);
-        });
-        actualDom.appendChild(fragment);
-        callback(actualToRemove, actualDom, manipulateDom); 
-    }
-    else {
-        var preToConvert = document.getElementsByTagName('pre')
-        var numberOfPres = preToConvert.length;
-
-        //change all pre's to new class
-        for(let x = 0; x < preToConvert.length ; x++) {
-            preToConvert[x].classList.add('language-XML');
-        }
-
-        //change tag from pre -> code
-        for(let x = 0; x < numberOfPres; x++) {
-            preToConvert[0].outerHTML = preToConvert[0].outerHTML.replace(/pre/g,"code");
-        }
-
-        for(let x = 0; x < numberOfPres ; x++)  {
-            tdArray[x].style.background = '#f5f2f0';
-        }
-
-        var s = document.createElement('script');
-        s.src = chrome.extension.getURL('prism.js');
-        (document.head||document.documentElement).appendChild(s);
-        s.onload = function() {
-            s.parentNode.removeChild(s);
-        };
-
-        return;
-    }
-}
-
-function removeActual(actualToRemove, actualDom, callback) {
-    //codify every other pre
-    actualToRemove.parentNode.removeChild(actualToRemove);
-    actualDom.classList.remove('failure');
-    actualDom.classList.add('rishiDiv');
-    counter++;
-    callback(removeActual);
-}
-
-
-},{"diff":2}],2:[function(require,module,exports){
 /*!
 
  diff v3.4.0
@@ -1934,4 +1838,100 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ ])
 });
 ;
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+var jsdiff = require('diff');
+var counter = 0;
+var orderedList = document.getElementsByTagName('ol')
+var tdArray = document.getElementsByTagName('td')
+var numFails = document.getElementsByClassName('failure').length - 1; //number of fails starting at 0 ;)
+var failures, myDivs = [];
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender) {
+        if (request.message == "removeDiff") {
+            var myDivs = [];
+            manipulateDom(removeActual);
+        }
+    }
+);
+
+function manipulateDom(callback) {
+
+    if (counter <= numFails) {
+
+        orderedList[0].parentNode.removeChild(orderedList[0]) //remove lists
+
+        var actualDom = document.getElementsByClassName('failure')[0]; //points to div
+
+        var actualToRemove = document.getElementsByClassName('failure')[0].getElementsByClassName('actual')[0]; //points to pre
+        var actual = document.getElementsByClassName('failure')[0].getElementsByClassName('actual')[0].textContent;
+        var expected = document.getElementsByClassName('failure')[0].getElementsByClassName('expected')[0].textContent;
+        document.getElementsByClassName('failure')[0].getElementsByClassName('expected')[0].style.textDecoration = "none"; //remove line-through expected
+
+        var actualDomArray = document.getElementsByClassName('failure'); //points to div
+        var color = '',
+            code = null;
+        var diff = jsdiff.diffLines(actual, expected, {"ignoreWhitespace": false, "newlineIsToken": false}),
+            description = document.getElementById('description'),
+
+            fragment = document.createDocumentFragment();
+        diff.forEach(function(part){
+            // green for additions, red for deletions
+            // grey for common parts
+            code = document.createElement('code');
+            color = part.added ? '#bbffbb' :
+                part.removed ? '#ffbbb0' : '#f5f2f0';
+            if (part.added || part.removed) {
+                code.classList.add('language-diff');
+            }
+            else {
+                code.classList.add('language-XML');
+            }
+            code.style.backgroundColor = color;
+            code.appendChild(document
+                .createTextNode(part.value));
+            fragment.appendChild(code);
+        });
+        actualDom.appendChild(fragment);
+        callback(actualToRemove, actualDom, manipulateDom); 
+    }
+    else {
+        var preToConvert = document.getElementsByTagName('pre')
+        var numberOfPres = preToConvert.length;
+
+        //change all pre's to new class
+        for(let x = 0; x < preToConvert.length ; x++) {
+            preToConvert[x].classList.add('language-XML');
+        }
+
+        //change tag from pre -> code
+        for(let x = 0; x < numberOfPres; x++) {
+            preToConvert[0].outerHTML = preToConvert[0].outerHTML.replace(/pre/g,"code");
+        }
+
+        for(let x = 0; x < numberOfPres ; x++)  {
+            tdArray[x].style.background = '#f5f2f0';
+        }
+
+        var s = document.createElement('script');
+        s.src = chrome.extension.getURL('prism.js');
+        (document.head||document.documentElement).appendChild(s);
+        s.onload = function() {
+            s.parentNode.removeChild(s);
+        };
+
+        return;
+    }
+}
+
+function removeActual(actualToRemove, actualDom, callback) {
+    //codify every other pre
+    actualToRemove.parentNode.removeChild(actualToRemove);
+    actualDom.classList.remove('failure');
+    actualDom.classList.add('rishiDiv');
+    counter++;
+    callback(removeActual);
+}
+
+
+},{"diff":1}]},{},[2]);
